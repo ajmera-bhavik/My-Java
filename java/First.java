1754b8dacb427fd50b2e5e477482c962d176676e
Ajmera Bhavik <ajmerabhavik7862@gmail.com>
2023-12-20 00:26:23 -1200

    Add New

--- java/First.java
+++ java/First.java
@@ -2,6 +2,8 @@ package com.java;
 
 public class First{
 	public static void main(String[] args) {
+		int a = 10;
+		System.out.println((a++) + "\t" +  (--a));
 		System.out.println("HEllo World");
 	}
 }
