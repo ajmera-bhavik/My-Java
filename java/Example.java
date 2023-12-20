223c6c41a5780c337ba06f216ac01bc481a2ffe1
Ajmera Bhavik <ajmerabhavik7862@gmail.com>
2023-12-20 00:32:13 -1200

    add new program

---
+++ java/Example.java
@@ -0,0 +1,19 @@
+package com.java;
+
+public class Example{
+public static void main(String[] args) {
+//	double x;
+//	x = Math.max(22, 33);
+//	System.out.println("The MaxiMum Among 22, 33 is " + x);
+//	x = Math.sqrt(49);
+//	System.out.println("The Square root of 49 is " + x);
+//	x = Math.abs(-55);
+//	System.out.println("The Absolute Value of - 55 is " + x);
+//	x = Math.pow(8, 4);
+//	System.out.println("The cube of 8 is Equal of  is " + x);
+//	x = Math.min(22,  33);
+//	System.out.println("The MiniMum among 22 and 33  is " + x);
+//	x = Math.log(3);
+//	System.out.println("The Natural log of 3 is " + x);
+	}
+}
